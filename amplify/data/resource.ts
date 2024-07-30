f32bddd4d94e26a466a367563e75c6689d636182
Geister Ralph <ralph.geister@wiedemann-group.com>
2024-07-30 10:21:43 +0200

    added per-user data isolation

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,4 +1,4 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+import { type ClientSchema, a, defineData } from '@aws-amplify/backend'
 
 /*== STEP 1 ===============================================================
 The section below creates a Todo database table with a "content" field. Try
@@ -9,30 +9,32 @@ specifies that any user authenticated via an API key can "create", "read",
 const schema = a.schema({
   Todo: a
     .model({
-      content: a.string(),
+      content: a.string()
     })
-    .authorization((allow) => [allow.publicApiKey()]),
-});
+    .authorization(allow => [allow.owner()])
+  //    .authorization((allow) => [allow.publicApiKey()]),
+})
 
-export type Schema = ClientSchema<typeof schema>;
+export type Schema = ClientSchema<typeof schema>
 
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    // API Key is used for a.allow.public() rules
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
-    },
-  },
-});
+    // defaultAuthorizationMode: 'apiKey',
+    // // API Key is used for a.allow.public() rules
+    // apiKeyAuthorizationMode: {
+    //   expiresInDays: 30
+    // }
+    defaultAuthorizationMode: 'userPool'
+  }
+})
 
 /*== STEP 2 ===============================================================
 Go to your frontend source code. From your client-side code, generate a
 Data client to make CRUDL requests to your table. (THIS SNIPPET WILL ONLY
 WORK IN THE FRONTEND CODE FILE.)
 
-Using JavaScript or Next.js React Server Components, Middleware, Server 
+Using JavaScript or Next.js React Server Components, Middleware, Server
 Actions or Pages Router? Review how to generate Data clients for those use
 cases: https://docs.amplify.aws/gen2/build-a-backend/data/connect-to-API/
 =========================================================================*/
