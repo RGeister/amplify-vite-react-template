a57a4eaae5e0a58d6111ad2d200f3ff59c910eac
Geister Ralph <ralph.geister@wiedemann-group.com>
2024-08-02 08:11:25 +0200

    add storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,14 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
+import { defineStorage } from '@aws-amplify/backend';
+
+export const storage = defineStorage({
+  name: 'amplifyTeamDrive'
+});
 
 defineBackend({
   auth,
   data,
+  storage
 });
