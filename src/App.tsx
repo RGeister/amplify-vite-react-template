a954d2a530ffed1f69e2e7d6c2c16f60703ae033
Geister Ralph <ralph.geister@wiedemann-group.com>
2024-08-21 17:30:47 +0200

    add function say-hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from 'react'
 import type { Schema } from '../amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
-import { Authenticator } from '@aws-amplify/ui-react'
+import {
+  Authenticator,
+  Badge,
+  Button,
+  Card,
+  Flex,
+  Image,
+  StepperField,
+  Text
+} from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
 const client = generateClient<Schema>()
@@ -9,10 +18,30 @@ const client = generateClient<Schema>()
 function App () {
   const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([])
 
+  // const fetchTodos = async () => {
+  //   const { data: items, errors } = await client.models.Todo.list()
+  //   console.log('errors', errors)
+  //   setTodos(items)
+  // }
+
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
+    // fetchTodos()
+    const sub = client.models.Todo.observeQuery().subscribe({
       next: data => setTodos([...data.items])
     })
+
+    // Subscribe to creation of Todo
+    const createSub = client.models.Todo.onCreate().subscribe({
+      next: data => console.log('ON CREATE:', data),
+      error: error => console.warn(error)
+    })
+
+    client.models.Todo.onCreate()
+
+    return () => {
+      createSub.unsubscribe()
+      sub.unsubscribe()
+    }
   }, [])
 
   function createTodo () {
@@ -24,6 +53,13 @@ function App () {
 
   function deleteTodo (id: string) {
     client.models.Todo.delete({ id })
+  }
+
+  function handleSayHello () {
+    const result = client.queries.sayHello({
+      name: 'Amplify'
+    })
+    console.log('say-Hello', result)
   }
 
   return (
@@ -48,6 +84,41 @@ function App () {
           </div>
 
           <button onClick={signOut}>Sign out</button>
+
+          <Card variation='elevated'>
+            <Flex alignItems='flex-start'>
+              <Image
+                src='/amplify-placeholder.svg'
+                alt='Amplify'
+                width='8rem'
+              />
+              <Flex direction='column' gap='xs'>
+                <Flex>
+                  <Badge variation='success'>New</Badge>
+                </Flex>
+                <Text fontSize='large' fontWeight='semibold'>
+                  Product title
+                </Text>
+                <Text color='font.tertiary'>Product description</Text>
+                <Text fontSize='large' color='secondary'>
+                  $199.99
+                </Text>
+                <Flex>
+                  <StepperField
+                    label='Quantity'
+                    min={0}
+                    max={10}
+                    step={1}
+                    defaultValue={1}
+                    labelHidden
+                  />
+                  <Button variation='primary' onClick={handleSayHello}>
+                    Add to cart
+                  </Button>
+                </Flex>
+              </Flex>
+            </Flex>
+          </Card>
         </main>
       )}
     </Authenticator>
