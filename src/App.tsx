8c5705323858418c0e2f76844b7f89d4f65c2754
Geister Ralph <ralph.geister@wiedemann-group.com>
2024-08-23 10:36:18 +0200

    merge conflict solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-// @ts-nocheck
 import { useEffect, useState } from 'react'
 import type { Schema } from '../amplify/data/resource'
 import { generateClient } from 'aws-amplify/data'
-<<<<<<< HEAD
-import { Authenticator, Button } from '@aws-amplify/ui-react'
-=======
 import {
   Authenticator,
   Badge,
@@ -15,76 +11,12 @@ import {
   StepperField,
   Text
 } from '@aws-amplify/ui-react'
->>>>>>> dev
 import '@aws-amplify/ui-react/styles.css'
-import { uploadData, list, downloadData  } from 'aws-amplify/storage'
-import ExampleComponent from './ExampleComponent'
 
 const client = generateClient<Schema>()
 
-// type AppProps = {
-//   signOut?: ((data?: any | undefined) => void) | undefined
-//   user?: AuthUser | undefined
-// }
-
-// function App () {
-//   const authFunc = ({ signOut, user }: AppProps): JSX.Element => {
-//     {
-//       console.log('user:', user)
-//       return <Main signOut={signOut} user={user} />
-//     }
-//   }
-
-//   return <Authenticator>{authFunc}</Authenticator>
-// }
-
-// const Main = ({ signOut, user }: AppProps) => {
-//   const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([])
-
-//   useEffect(() => {
-//     client.models.Todo.observeQuery().subscribe({
-//       next: data => setTodos([...data.items])
-//     })
-//   }, [])
-
-//   function createTodo () {
-//     client.models.Todo.create({
-//       content: window.prompt('Todo content'),
-//       isDone: false
-//     })
-//   }
-
-//   function deleteTodo (id: string) {
-//     client.models.Todo.delete({ id })
-//   }
-
-//   return (
-//     <main>
-//       <h1>{user?.signInDetails?.loginId}'s todos</h1>
-//       <button onClick={createTodo}>+ new</button>
-//       <ul>
-//         {todos.map(todo => (
-//           <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-//             {todo.content}
-//           </li>
-//         ))}
-//       </ul>
-//       <div>
-//         🥳 App successfully hosted. Try creating a new todo.
-//         <br />
-//         <a href='https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates'>
-//           Review next step of this tutorial.
-//         </a>
-//       </div>
-
-//       <button onClick={signOut}>Sign out</button>
-//     </main>
-//   )
-// }
-
 function App () {
   const [todos, setTodos] = useState<Array<Schema['Todo']['type']>>([])
-  const [files, setFiles] = useState([])
 
   const fetchTodos = async () => {
     const { data: items, errors } = await client.models.Todo.list()
@@ -113,11 +45,6 @@ function App () {
       next: data => setTodos([...data.items])
     })
 
-<<<<<<< HEAD
-    // client.queries.sayHello({
-    //   name: 'Amplify'
-    // })
-=======
     // Subscribe to creation of Todo
     const createSub = client.models.Todo.onCreate().subscribe({
       next: data => console.log('ON CREATE:', data),
@@ -132,46 +59,19 @@ function App () {
       createSub.unsubscribe()
       sub.unsubscribe()
     }
->>>>>>> dev
   }, [])
 
   function createTodo () {
-    client.models.Todo.create({ content: window.prompt('Todo content') })
+    client.models.Todo.create({
+      content: window.prompt('Todo content'),
+      isDone: false
+    })
   }
 
   function deleteTodo (id: string) {
     client.models.Todo.delete({ id })
   }
 
-<<<<<<< HEAD
-  const [file, setFile] = useState<File>()
-
-  const handleChange = (event: any) => {
-    setFile(event.target.files[0])
-  }
-
-  const listFiles = async () => {
-    const result = await list({
-      path: 'picture-submissions/'
-      // Alternatively, path: ({identityId}) => `album/{identityId}/photos/`
-    })
-    console.log('FILES:', result)
-    setFiles(result)
-  }
-
-  const downloadFile = async () => {
-    try {
-      const downloadResult = await downloadData({
-        path: files.items[0].path
-      }).result
-      const text = await downloadResult.body.text()
-      // Alternatively, you can use `downloadResult.body.blob()`
-      // or `downloadResult.body.json()` get read body in Blob or JSON format.
-      console.log('Succeed: ', text)
-    } catch (error) {
-      console.log('Error : ', error)
-    }
-=======
   async function handleSayHello () {
     const result = await client.queries.sayHello({
       name: 'Amplify'
@@ -188,7 +88,6 @@ function App () {
 
   async function handleUpdate () {
     fetchTodos()
->>>>>>> dev
   }
 
   return (
@@ -212,23 +111,6 @@ function App () {
             </a>
           </div>
 
-          <div>
-            <input type='file' onChange={handleChange} />
-            <Button
-              onClick={() =>
-                uploadData({
-                  path: `picture-submissions/${file?.name}`,
-                  data: file
-                })
-              }
-            >
-              Upload
-            </Button>
-            <Button onClick={listFiles}>List Files</Button>
-            <Button onClick={downloadFile}>Download File</Button>
-          </div>
-
-              <ExampleComponent />
           <button onClick={signOut}>Sign out</button>
 
           <Card variation='elevated'>
