7ed734ee65ddc9e22e4cdc7fcc42f249542a1076
Geister Ralph <ralph.geister@wiedemann-group.com>
2024-08-28 15:16:25 +0200

    get-user changes

--- amplify/functions/get-user/handler.ts
+++ amplify/functions/get-user/handler.ts
@@ -1,26 +1,30 @@
-import { generateClient } from 'aws-amplify/data'
+// import { generateClient } from 'aws-amplify/data'
 import type { Schema } from "../../data/resource"
-
+// import { Amplify } from "aws-amplify";
+// import outputs from "../../../amplify_outputs.json";
 
 //@ts-ignore
 import { executeQuery } from './query';
 
-const client = generateClient<Schema>()
+// Amplify.configure(outputs);
+// const client = generateClient<Schema>()
+
 
 export const handler: Schema["getUser"]["functionHandler"] = async (event) => {
   // // arguments typed from `.arguments()`
   const { id } = event.arguments
-  // // return typed from `.returns()`
-  // const result = await executeQuery(event)
-  // console.log("get-user finished");
+
+  const result = await executeQuery(event)
+  console.log("get-user finished!", id);
 
 
-  client.models.Todo.create({
-    content: "MY NEW CONTENT",
-    isDone: false
-  })
+  // const res = await client.models.Todo.create({
+  //   content: "MY NEW CONTENT",
+  //   isDone: false
+  // })
 
+  // console.log("create result", res);
 
-  return { id, name: 'Tom' }
-  // return result;
+  // return { id, name: 'Tom' }
+  return result;
 }
